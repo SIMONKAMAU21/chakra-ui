4c38e41fa59090fcd65775480819292075d06090
simon kamau <https://github.com/SIMONKAMAU21/own-git-.git>
2024-05-18 19:48:49 +0300

    designed the about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,93 @@
 import React from 'react'
 import Images from '../components/images/Images'
+import { Box, Card, CardBody, CardFooter, CardHeader, Flex, Heading, Image, Tab, TabList, TabPanel, TabPanels, Tabs, Text } from '@chakra-ui/react'
 
 const About = () => {
+  const heading = {
+    fontSize: {
+      base: '2px',
+      md: "18px",
+      sm: "10px"
+    },
+    overflow: "hidden",
+    alignItems:"center",
+    justifyContent:'space-between',
+    display:"flex",
+    w:{
+      base:'40%',
+      md:"50%",
+      sm:"30%"
+    },
+    h:{
+      base:"40%",
+      md:'30%',
+      sm:"30%"
+    }
+  }
+  const image={
+    w:{
+      base:"350px",
+      md:'550px',
+      sm:"250px"
+    },
+    h:{
+      base:"350px",
+      md:'350px',
+      sm:"250px"
+    }
+  }
+
   return (
-    <div>
-      <Images/>
-    </div>
+    <Box h='100vh' w='70vw' >
+      <Tabs>
+        <TabList>
+          <Tab>Blogs</Tab>
+          <Tab>Images</Tab>
+        </TabList>
+
+        <TabPanels>
+          <TabPanel>
+            <Flex gap='10px'display={heading.display} justifyContent={heading.justifyContent} >
+              <Card sx={heading}>
+                <CardHeader>
+                  <Heading fontSize={heading.fontSize}>Lorem, ipsum dolor sit amet </Heading>
+                </CardHeader>
+                <CardBody>
+                  <Flex gap='10px'>
+                    <Image src='https://picsum.photos/200/300?random=1' alt='my image' sx={image}/>
+                    <Image src='https://picsum.photos/200/300?random=2' alt='my image' sx={image}/>
+                  </Flex>
+                </CardBody>
+                <CardFooter>
+                  <Text>Lorem ipsum dolor sit, amet consectetur adipisicing elit. In numquam ullam dolore enim impedit est vero culpa assumenda voluptates iure? Aut doloribus, culpa inventore quas accusantium eaque minus neque excepturi expedita! Nemo earum officia corrupti dolor sit fugiat asperiores aspernatur voluptatum ipsam temporibus dignissimos, perspiciatis vero aperiam eos repudiandae modi magnam natus iusto maxime excepturi neque. Impedit voluptatibus sit provident qui aspernatur vitae, ipsa quos amet earum delectus aliquam vel?</Text>
+                </CardFooter>
+              </Card>
+              <Card sx={heading}>
+                <CardHeader>
+                  <Heading fontSize={heading.fontSize}>Lorem, ipsum dolor sit amet </Heading>
+                </CardHeader>
+                <CardBody>
+                  <Flex gap='10px'>
+                    <Image src='https://picsum.photos/200/300?random=3' alt='my image' w='650px' h='350px'/>
+                    <Image src='https://picsum.photos/200/300?random=4' alt='my image' w='650px' h='350px'/>
+                  </Flex>
+                </CardBody>
+                <CardFooter>
+                  <Text>Lorem ipsum dolor sit, amet consectetur adipisicing elit. In numquam ullam dolore enim impedit est vero culpa assumenda voluptates iure? Aut doloribus, culpa inventore quas accusantium eaque minus neque excepturi expedita! Nemo earum officia corrupti dolor sit fugiat asperiores aspernatur voluptatum ipsam temporibus dignissimos, perspiciatis vero aperiam eos repudiandae modi magnam natus iusto maxime excepturi neque. Impedit voluptatibus sit provident qui aspernatur vitae, ipsa quos amet earum delectus aliquam vel?</Text>
+                </CardFooter>
+              </Card>
+
+     
+
+
+            </Flex>
+          </TabPanel>
+          <TabPanel>
+            <Images />
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
+    </Box>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { Avatar, AvatarGroup, Box,  Text ,useColorMode, useTheme} from '@chakra-ui/react';
+import { Accordion, AccordionButton, AccordionIcon, AccordionItem, AccordionPanel, Avatar, AvatarGroup, Box,  Image,  Text ,useColorMode, useTheme} from '@chakra-ui/react';
 import React from 'react';
 import Images from '../components/images/Images';
 
@@ -10,23 +10,81 @@ const Home = () => {
 
   return (
     <Box >
-      <Box bg={isDark?"orange.300":'orange.300'} p='10px'>
-        <Text>
+      <Accordion allowToggle>
+      <AccordionItem>
+      <h2>
+      <AccordionButton _expanded={{bg:"orange.400"}}>
+      <Box flex='1' textAlign='left'>
+      see more
+      </Box>
+      <AccordionIcon/>
+      </AccordionButton>
+      </h2>
+      <AccordionPanel>
+        
+        <Box bg={isDark?"":'gray.50'} p='10px'>
+        <Text bg={isDark?"":'gray.150'} p='10px' >
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. A odit quos sapiente rerum suscipit repellendus doloremque dolores aspernatur illum harum dolor iste deserunt omnis consectetur consequuntur, cumque ratione! At, corrupti?
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nulla odit error in quidem at culpa, perferendis tempore ipsum magni debitis sint facilis molestiae earum impedit ut exercitationem fugiat laudantium eos cupiditate deserunt aut nam repellendus. Provident ullam corrupti adipisci voluptas, sed eum quo natus accusamus fugiat, excepturi temporibus nostrum id?
-        </Text>
-        <hr />
-        <Text>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos unde inventore nisi harum, eaque veniam atque aliquid eum quas deserunt magnam, vel aut quisquam fuga alias quia optio saepe est, voluptates et quam corporis accusamus praesentium temporibus! Aliquam quas possimus et ab pariatur voluptate dolorem molestias doloribus reprehenderit autem nemo sit quaerat repellendus illum, quasi alias iste enim voluptatem. Illo iusto et repellat quasi aperiam accusamus libero animi at placeat deserunt numquam eligendi corporis debitis sed earum iure enim porro, incidunt facere eveniet ex unde magnam nam? Enim, non? Quasi corporis quidem magnam iure iusto, corrupti eligendi et veniam distinctio reiciendis temporibus illo placeat voluptatibus doloremque neque rerum provident amet id, deserunt voluptatum fuga esse nemo nesciunt ratione? Asperiores, veritatis pariatur id voluptas quam quasi. Pariatur accusantium numquam sint ipsa expedita eligendi incidunt aliquam obcaecati, exercitationem autem aperiam error iusto? Quibusdam, nulla alias? Aspernatur magnam veniam sit reiciendis magni commodi voluptatem itaque libero! Libero architecto, qui ipsam temporibus minima debitis porro ipsum! Ipsum corporis officiis quo saepe id assumenda! Cumque soluta suscipit fugit itaque dolores praesentium perferendis labore, nobis et numquam qui magnam excepturi nesciunt dicta deserunt tempora, incidunt minus expedita enim eligendi. Repellat temporibus explicabo, voluptate voluptatibus minus vitae illo error totam excepturi neque, facere perspiciatis asperiores vero consequatur a voluptatum! Totam, corporis. Autem omnis, quia eius quibusdam, fugiat laborum placeat vero praesentium quasi distinctio officia culpa ut rerum aspernatur, dignissimos blanditiis a expedita! Rem hic cumque voluptates est enim tempora totam quidem cum excepturi, tempore quam corrupti porro, dolorem facilis itaque, error architecto? Magni ipsam, praesentium perferendis rem neque quibusdam corrupti pariatur eaque in error deserunt quam. Tempora officia culpa rem blanditiis odit beatae non. Architecto laudantium aspernatur sint assumenda, tempore est minima rerum ipsa illo molestiae accusantium doloribus dolorum quidem reprehenderit facilis magni quaerat id ipsam dolore, eligendi eos fuga in quod aliquam. Adipisci enim necessitatibus aperiam consequatur, dolores molestiae laboriosam at accusantium nemo. Veniam omnis cupiditate numquam sit atque ipsum doloribus asperiores libero. Alias vel et porro eveniet, eos autem repellendus totam magni tempora, unde, dignissimos perspiciatis hic quaerat numquam illo inventore! Dolor quisquam consequuntur quibusdam saepe tempore corrupti quaerat eligendi eum. Reiciendis blanditiis eos accusantium sint? Exercitationem odio delectus numquam, deserunt a eligendi quo molestias autem quasi enim alias iste veniam et earum praesentium laboriosam dignissimos commodi nulla fugiat modi! Blanditiis aut sunt quisquam illo, dolor placeat porro voluptate provident corporis! Beatae aut eaque facilis dicta itaque sed totam sunt dolorum quibusdam! Officiis dolor a facere possimus soluta repellendus minus ipsum eius. Exercitationem voluptatibus iste, culpa perferendis molestiae sequi ipsa incidunt nisi non! Et debitis, alias voluptatem accusamus fugit tenetur a dolore! Blanditiis omnis eos aliquid maiores? Excepturi corporis, voluptatem aliquid illum est doloribus asperiores ex nam totam veniam molestiae officiis exercitationem ratione iusto modi delectus! Veritatis corporis voluptatibus ea explicabo delectus saepe ab. Odio illum exercitationem vel sunt! Esse temporibus eaque in dolorum necessitatibus laudantium molestiae ratione. Fuga, dolores perspiciatis iure itaque dignissimos optio praesentium ducimus? Exercitationem molestias non dolores aperiam facere eaque earum.
-        </Text>
+        </Text>        
         <AvatarGroup>
           <Avatar name='simon kamau' src='https://picsum.photos/200/300?random=5' />
           <Avatar name='peninah gatuma'src='https://picsum.photos/200/300?random=6' />
           <Avatar name='naomi gatuma'src='https://picsum.photos/200/300?random=7' />
         </AvatarGroup>
+       <Box display='flex' gap='10px' mt='10px' justifyContent='space-between'>
+       <Image src='https://picsum.photos/200/300?random=1' w='350px' h='350px'/>
+        <Image src='https://picsum.photos/200/300?random=2'w='350px' h='350px'/>
+        <Image src='https://picsum.photos/200/300?random=3'w='350px' h='350px'/>
+        <Image src='https://picsum.photos/200/300?random=4'w='350px' h='350px'/>
+        <Image src='https://picsum.photos/200/300?random=5'w='350px' h='350px'/>
+        <Image src='https://picsum.photos/200/300?random=6'w='350px' h='350px'/>
+        <Image src='https://picsum.photos/200/300?random=7'w='350px' h='350px'/>
+       </Box>
       </Box>
-   
-      <Images/>
-    </Box>
+      </AccordionPanel>
+      </AccordionItem>
+      
+      
+      </Accordion>
+      <Accordion allowToggle>
+      <AccordionItem>
+      <h2>
+      <AccordionButton _expanded={{bg:"orange.400"}}>
+      <Box flex='1' textAlign='left'>
+      see more
+      </Box>
+      <AccordionIcon/>
+      </AccordionButton>
+      </h2>
+      <AccordionPanel>
+        
+        <Box bg={isDark?"":'gray.50'} p='10px'>
+        <Text bg={isDark?"":'gray.150'} p='10px' >
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. A odit quos sapiente rerum suscipit repellendus doloremque dolores aspernatur illum harum dolor iste deserunt omnis consectetur consequuntur, cumque ratione! At, corrupti?
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nulla odit error in quidem at culpa, perferendis tempore ipsum magni debitis sint facilis molestiae earum impedit ut exercitationem fugiat laudantium eos cupiditate deserunt aut nam repellendus. Provident ullam corrupti adipisci voluptas, sed eum quo natus accusamus fugiat, excepturi temporibus nostrum id?
+        </Text>        
+        <AvatarGroup>
+          <Avatar name='simon kamau' src='https://picsum.photos/200/300?random=5' />
+          <Avatar name='peninah gatuma'src='https://picsum.photos/200/300?random=6' />
+          <Avatar name='naomi gatuma'src='https://picsum.photos/200/300?random=7' />
+        </AvatarGroup>
+       <Box display='flex' gap='10px' mt='10px' justifyContent='space-between'>
+       <Image src='https://picsum.photos/200/300?random=1' w='350px' h='350px'/>
+        <Image src='https://picsum.photos/200/300?random=2'w='350px' h='350px'/>
+        <Image src='https://picsum.photos/200/300?random=3'w='350px' h='350px'/>
+        <Image src='https://picsum.photos/200/300?random=4'w='350px' h='350px'/>
+        <Image src='https://picsum.photos/200/300?random=5'w='350px' h='350px'/>
+        <Image src='https://picsum.photos/200/300?random=6'w='350px' h='350px'/>
+        <Image src='https://picsum.photos/200/300?random=7'w='350px' h='350px'/>
+       </Box>
+      </Box>
+      </AccordionPanel>
+      </AccordionItem>
+      
+      
+      </Accordion>
+       </Box>
   );
 };
 
