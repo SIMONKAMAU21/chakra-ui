db5901dfee376972f8daaa32fea9ad794648f99c
simon kamau <https://github.com/SIMONKAMAU21/own-git-.git>
2024-05-19 21:15:13 +0300

    added design on about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -9,7 +9,6 @@ const About = () => {
       md: "18px",
       sm: "10px"
     },
-    overflow: "hidden",
     alignItems:"center",
     flex:"1",
     justifyContent:'space-between',
@@ -64,6 +63,7 @@ const About = () => {
                   <Text>Lorem ipsum dolor sit, amet consectetur adipisicing elit. In numquam ullam dolore enim impedit est vero culpa assumenda voluptates iure? Aut doloribus, culpa inventore quas accusantium eaque minus neque excepturi expedita! Nemo earum officia corrupti dolor sit fugiat asperiores aspernatur voluptatum ipsam temporibus dignissimos, perspiciatis vero aperiam eos repudiandae modi magnam natus iusto maxime excepturi neque. Impedit voluptatibus sit provident qui aspernatur vitae, ipsa quos amet earum delectus aliquam vel?</Text>
                 </CardFooter>
               </Card>
+
               <Card sx={heading}>
                 <CardHeader>
                   <Heading fontSize={heading.fontSize}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam alias repudiandae, iure dicta similique ea dignissimos corporis perferendis quasi assumenda. </Heading>
@@ -83,6 +83,17 @@ const About = () => {
           </TabPanel>
           <TabPanel>
             <Images />
+            <Card>
+              <CardHeader>
+                <Text>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Est rem in itaque ipsa, incidunt, sapiente neque magni dolor atque beatae fuga voluptatem nisi tempora sed. Alias asperiores laudantium nam eum.
+                </Text>
+              </CardHeader>
+              <CardBody>
+                <Image/>
+              </CardBody>
+              <CardFooter></CardFooter>
+            </Card>
           </TabPanel>
         </TabPanels>
       </Tabs>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -57,14 +57,6 @@ const Home = () => {
               </Box>
             </AccordionPanel>
           </AccordionItem>
-        </Accordion>
-      </Card>
-      <Divider />
-      <Card>
-        <Text p='10px'>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam dolore ea quae, atque eaque repudiandae accusantium delectus velit quasi animi soluta nemo laboriosam repellat consequatur aspernatur, minima non quia, blanditiis architecto possimus voluptas a facilis magnam. Adipisci, provident voluptas tempore nostrum laboriosam voluptatibus inventore minima quas sunt quibusdam saepe eos quaerat consequuntur repellat voluptatum similique repudiandae ipsum enim qui eligendi.
-        </Text>
-        <Accordion allowToggle>
           <AccordionItem>
             <h2>
               <AccordionButton _expanded={{ bg: "green.100" }}>
@@ -93,6 +85,48 @@ const Home = () => {
                   <Image src='https://picsum.photos/200/300?random=5' w='350px' h='350px' />
                   <Image src='https://picsum.photos/200/300?random=6' w='350px' h='350px' />
                   <Image src='https://picsum.photos/200/300?random=7' w='350px' h='350px' />
+                </Box>
+              </Box>
+            </AccordionPanel>
+          </AccordionItem>
+        </Accordion>
+      </Card>
+
+      <Divider />
+
+      <Card>
+        <Text p='10px'>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam dolore ea quae, atque eaque repudiandae accusantium delectus velit quasi animi soluta nemo laboriosam repellat consequatur aspernatur, minima non quia, blanditiis architecto possimus voluptas a facilis magnam. Adipisci, provident voluptas tempore nostrum laboriosam voluptatibus inventore minima quas sunt quibusdam saepe eos quaerat consequuntur repellat voluptatum similique repudiandae ipsum enim qui eligendi.
+        </Text>
+        <Accordion allowToggle>
+        <AccordionItem>
+            <h2>
+              <AccordionButton _expanded={{ bg: "green.100" }}>
+                <Box flex='1' textAlign='left' fontSize='20px' color={isDark ? 'orange' : "blue"}>
+                  see more
+                </Box>
+                <AccordionIcon />
+              </AccordionButton>
+            </h2>
+            <AccordionPanel>
+              <Box bg={isDark ? "" : 'gray.50'} p='10px'>
+                <Text bg={isDark ? "" : 'gray.150'} p='10px'>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. A odit quos sapiente rerum suscipit repellendus doloremque dolores aspernatur illum harum dolor iste deserunt omnis consectetur consequuntur, cumque ratione! At, corrupti?
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nulla odit error in quidem at culpa, perferendis tempore ipsum magni debitis sint facilis molestiae earum impedit ut exercitationem fugiat laudantium eos cupiditate deserunt aut nam repellendus. Provident ullam corrupti adipisci voluptas, sed eum quo natus accusamus fugiat, excepturi temporibus nostrum id?
+                </Text>
+                <AvatarGroup>
+                  <Avatar name='simon kamau' src='https://picsum.photos/200/300?random=5' />
+                  <Avatar name='peninah gatuma' src='https://picsum.photos/200/300?random=6' />
+                  <Avatar name='naomi gatuma' src='https://picsum.photos/200/300?random=7' />
+                </AvatarGroup>
+                <Box sx={scrollBoxStyles}>
+                  <Image src='https://picsum.photos/200/300?random=11' w='350px' h='350px' />
+                  <Image src='https://picsum.photos/200/300?random=12' w='350px' h='350px' />
+                  <Image src='https://picsum.photos/200/300?random=13' w='350px' h='350px' />
+                  <Image src='https://picsum.photos/200/300?random=14' w='350px' h='350px' />
+                  <Image src='https://picsum.photos/200/300?random=15' w='350px' h='350px' />
+                  <Image src='https://picsum.photos/200/300?random=16' w='350px' h='350px' />
+                  <Image src='https://picsum.photos/200/300?random=17' w='350px' h='350px' />
                 </Box>
               </Box>
             </AccordionPanel>
