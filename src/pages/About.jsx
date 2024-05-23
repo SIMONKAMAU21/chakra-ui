e0c88c6114529500670b9e9b69f039dd76bde1cf
simon kamau <https://github.com/SIMONKAMAU21/own-git-.git>
2024-05-23 21:26:28 +0300

    redisigned the home page and about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -15,16 +15,24 @@ import {
   TabPanels,
   Tabs,
   Text,
-  VStack
+  VStack,
+  Stack,
+  Badge,
+  SimpleGrid,
+  Divider,
 } from '@chakra-ui/react';
-import simon from '../assets/simo.jpg'
+import { motion } from 'framer-motion';
+import simon from '../assets/simo.jpg';
+
+const MotionBox = motion(Box);
+const MotionImage = motion(Image);
 
 const About = () => {
   const headingStyles = {
     fontSize: {
-      base: '16px',
-      md: '18px',
-      lg: '20px'
+      base: '20px',
+      md: '22px',
+      lg: '24px'
     },
     textAlign: 'center',
     w: '100%'
@@ -36,70 +44,118 @@ const About = () => {
       md: '45%'
     },
     h: 'auto',
-    objectFit: 'cover'
+    objectFit: 'cover',
+    borderRadius: 'md',
+    whileHover: { scale: 1.05 },
+    transition: { duration: 0.3 }
   };
 
   return (
-    <Box h='100vh' w={{ base: '100vw', md: '80vw' }} p={4} overflowY='auto' >
-      <Tabs>
-        <TabList>
-          <Tab>Blogs</Tab>
-          <Tab>Images</Tab>
+    <Box h='100vh' w={{ base: '100vw', md: '80vw' }} p={4} overflowY='auto'>
+      <Tabs isFitted variant="enclosed">
+        <TabList mb="1em">
+          <Tab _selected={{ color: 'white', bg: 'blue.500' }}>About</Tab>
+          <Tab _selected={{ color: 'white', bg: 'blue.500' }}>Images</Tab>
         </TabList>
 
-        <TabPanels >
+        <TabPanels>
           <TabPanel>
             <VStack spacing={6}>
-              <Card w='100%'>
+              <Card w='100%' boxShadow='lg'>
                 <CardHeader>
                   <Heading sx={headingStyles}>
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam dolorem aliquam sed voluptatibus minima quia neque odit iusto molestiae. Minus!
+                    About Me
                   </Heading>
                 </CardHeader>
                 <CardBody>
-                  <Flex flexDirection={{ base: 'column', md: 'row' }} gap='10px'>
-                    <Image src='https://picsum.photos/200/300?random=1' alt='image 1' sx={imageStyles} />
-                    <Image src={simon
-                    } alt='image 2' sx={imageStyles} />
+                  <Flex flexDirection={{ base: 'column', md: 'row' }} alignItems='center' gap='10px'>
+                    <MotionImage src={simon} alt='Simon Kamau' sx={imageStyles} />
+                    <Text fontSize='lg'>
+                      Hi, I'm Simon Kamau, a passionate Full Stack Developer with expertise in creating interactive web applications. With a strong background in both front-end and back-end development, I enjoy building solutions that are not only functional but also visually appealing. My journey in software development has equipped me with a diverse set of skills and the ability to adapt to new technologies quickly.
+                    </Text>
                   </Flex>
                 </CardBody>
-                <CardFooter>
-                  <Text>
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. In numquam ullam dolore enim impedit est vero culpa assumenda voluptates iure? Aut doloribus, culpa inventore quas accusantium eaque minus neque excepturi expedita!
-                  </Text>
-                </CardFooter>
               </Card>
 
-              <Card w='100%'>
+              <Card w='100%' boxShadow='lg'>
                 <CardHeader>
                   <Heading sx={headingStyles}>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam alias repudiandae, iure dicta similique ea dignissimos corporis perferendis quasi assumenda.
+                    Skills
+                  </Heading>
+                </CardHeader>
+                <CardBody>
+                  <SimpleGrid columns={{ base: 1, md: 2 }} spacing={10}>
+                    <Stack spacing={3}>
+                      <Badge bg='orange.400'>JavaScript</Badge>
+                      <Badge bg='orange.400'>Node.js</Badge>
+                      <Badge bg='orange.400'>Express</Badge>
+                      <Badge bg='orange.400'>React</Badge>
+                      <Badge bg='orange.400'>MongoDB</Badge>
+                    </Stack>
+                    <Stack spacing={3}>
+                      <Badge bg='orange.400'>HTML</Badge>
+                      <Badge bg='orange.400'>CSS</Badge>
+                      <Badge bg='orange.400'>Chakra UI</Badge>
+                      <Badge bg='orange.400'>Git</Badge>
+                      <Badge bg='orange.400'>Docker</Badge>
+                    </Stack>
+                  </SimpleGrid>
+                </CardBody>
+              </Card>
+
+              <Card w='100%' boxShadow='lg'>
+                <CardHeader>
+                  <Heading sx={headingStyles}>
+                    Projects
+                  </Heading>
+                </CardHeader>
+                <CardBody>
+                  <SimpleGrid columns={{ base: 1, md: 2 }} spacing={10}>
+                    <Box>
+                      <Heading size='md'>Project 1</Heading>
+                      <Text mt={2}>
+                        A brief description of the project. Highlight key features, technologies used, and the impact it had.
+                      </Text>
+                    </Box>
+                    <Box>
+                      <Heading size='md'>Project 2</Heading>
+                      <Text mt={2}>
+                        A brief description of the project. Highlight key features, technologies used, and the impact it had.
+                      </Text>
+                    </Box>
+                  </SimpleGrid>
+                </CardBody>
+              </Card>
+
+              <Divider />
+
+              <Card w='100%' boxShadow='lg'>
+                <CardHeader>
+                  <Heading sx={headingStyles}>
+                    Blog Preview
                   </Heading>
                 </CardHeader>
                 <CardBody>
                   <Flex flexDirection={{ base: 'column', md: 'row' }} gap='10px'>
-                    <Image src='https://picsum.photos/200/300?random=3' alt='image 3' sx={imageStyles} />
-                    <Image src='https://picsum.photos/200/300?random=4' alt='image 4' sx={imageStyles} />
+                    <MotionImage src='https://picsum.photos/200/300?random=1' alt='Blog 1' sx={imageStyles} />
+                    <Text fontSize='lg'>
+                      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam dolorem aliquam sed voluptatibus minima quia neque odit iusto molestiae. Minus!
+                    </Text>
                   </Flex>
                 </CardBody>
-                <CardFooter>
-                  <Text>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit dolore, voluptatum nesciunt accusantium natus tempora. Quidem accusantium, est, ducimus optio eius nesciunt aut fugiat, consequuntur corrupti maxime magnam veniam doloremque adipisci hic delectus veritatis quod repellendus consequatur ratione vero explicabo.
-                  </Text>
-                </CardFooter>
               </Card>
             </VStack>
           </TabPanel>
           <TabPanel>
             <Images />
-            <Card w='100%'>
+            <Card w='100%' boxShadow='lg'>
               <CardHeader>
                 <Text>
                   Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate expedita ipsam cumque, esse fugit assumenda a ullam ut veritatis quod itaque unde ad quo sit magni. Esse consequatur labore repellat?
                 </Text>
               </CardHeader>
               <CardBody>
-                <Image src='https://picsum.photos/200/300?random=5' alt='image 5' sx={imageStyles} />
+                <MotionImage src='https://picsum.photos/200/300?random=5' alt='image 5' sx={imageStyles} />
               </CardBody>
               <CardFooter />
             </Card>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,115 +1,66 @@
 import React from 'react';
 import {
-  Accordion,
-  AccordionButton,
-  AccordionIcon,
-  AccordionItem,
-  AccordionPanel,
-  Avatar,
-  AvatarGroup,
   Box,
-  Card,
-  Divider,
   Image,
   Text,
-  useColorMode
+  useColorMode,
+  VStack,
+  Heading,
+  HStack,
+  Link,
+  IconButton,
 } from '@chakra-ui/react';
-import simon from '../assets/simo.jpg'
+import { FaGithub, FaLinkedin, FaEnvelope } from 'react-icons/fa';
+import { motion } from 'framer-motion';
+import simon from '../assets/simo.jpg';
 
-const scrollBoxStyles = {
-  display: 'flex',
-  gap: '10px',
-  mt: '10px',
-  justifyContent: 'space-between',
-  overflow: 'auto',
-  '::-webkit-scrollbar': {
-    width: '0px',
-  },
-  '-ms-overflow-style': 'none',
-  'scrollbar-width': 'none',
-};
-
-const AccordionContent = ({ isDark }) => (
-  <Box bg={isDark ? "" : 'gray.50'} p='0px'>
-    <Text bg={isDark ? "" : 'gray.150'} p='10px'>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. A odit quos sapiente rerum suscipit repellendus doloremque dolores aspernatur illum harum dolor iste deserunt omnis consectetur consequuntur, cumque ratione! At, corrupti?
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nulla odit error in quidem at culpa, perferendis tempore ipsum magni debitis sint facilis molestiae earum impedit ut exercitationem fugiat laudantium eos cupiditate deserunt aut nam repellendus. Provident ullam corrupti adipisci voluptas, sed eum quo natus accusamus fugiat, excepturi temporibus nostrum id?
-    </Text>
-    <AvatarGroup>
-      <Avatar name='simon kamau' src={simon} />
-      <Avatar name='peninah gatuma' src='https://picsum.photos/200/300?random=6' />
-      <Avatar name='naomi gatuma' src='https://picsum.photos/200/300?random=7' />
-    </AvatarGroup>
-    <Box sx={scrollBoxStyles}>
-      {[...Array(7)].map((_, index) => (
-        <Image key={index} src={`https://picsum.photos/200/300?random=${8 + index}`} w='350px' h='350px' />
-      ))}
-    </Box>
-  </Box>
-);
+const MotionBox = motion(Box);
+const MotionImage = motion(Image);
 
 const Home = () => {
   const { colorMode } = useColorMode();
   const isDark = colorMode === 'dark';
   return (
-    <Box gap='10px' display='flex' flexDirection='column' w={{ base: "100vw", md: "80vw", sm: '100vw' }} p={{ base: "10px", md: "10px" }}>
-      <Card w="100%">
-        <Text p='10px'>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis alias optio, maxime, officiis suscipit nam eveniet quam perferendis, facere quos inventore. Deleniti optio, nisi itaque inventore quisquam libero eum ut recusandae aspernatur, sint, in eveniet maiores laudantium id. Ad praesentium accusamus aut minus inventore blanditiis beatae, possimus cumque vero iusto!
+    <MotionBox
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 1 }}
+      display='flex'
+      flexDirection='column'
+      alignItems='center'
+      justifyContent='center'
+      minHeight='100vh'
+      p={{ base: '10px', md: '20px' }}
+      bg={isDark ? 'gray.800' : 'gray.100'}
+    >
+      <VStack spacing='20px'>
+        <MotionImage
+          borderRadius='full'
+          boxSize={{ base: '200px', md: '300px' }}
+          src={simon}
+          alt='Simon Kamau'
+          whileHover={{ scale: 1.1 }}
+          transition={{ duration: 0.3 }}
+        />
+        <Heading as='h1' size='2xl'>
+          Simon Kamau
+        </Heading>
+        <Text fontSize='lg' textAlign='center'>
+          Full Stack Developer passionate about creating interactive applications and experiences on the web.
         </Text>
-        <Accordion allowToggle>
-          <AccordionItem>
-            <h2>
-              <AccordionButton _expanded={{ bg: "orange.400" }}>
-                <Box as='span' color={isDark ? 'white' : 'black'} flex='1' textAlign='left' fontSize='20px'>
-                  see more
-                </Box>
-                <AccordionIcon color={isDark ? 'orange' : 'blue'} />
-              </AccordionButton>
-            </h2>
-            <AccordionPanel>
-              <AccordionContent isDark={isDark} />
-            </AccordionPanel>
-          </AccordionItem>
-          <AccordionItem>
-            <h2>
-            <AccordionButton _expanded={{ bg: "orange.400" }}>
-                <Box flex='1' textAlign='left' fontSize='20px' color={isDark ? 'white' : "black"}>
-                  see more
-                </Box>
-                <AccordionIcon />
-              </AccordionButton>
-            </h2>
-            <AccordionPanel>
-              <AccordionContent isDark={isDark} />
-            </AccordionPanel>
-          </AccordionItem>
-        </Accordion>
-      </Card>
-
-      <Divider />
-
-      <Card w="100%">
-        <Text p='10px'>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam dolore ea quae, atque eaque repudiandae accusantium delectus velit quasi animi soluta nemo laboriosam repellat consequatur aspernatur, minima non quia, blanditiis architecto possimus voluptas a facilis magnam. Adipisci, provident voluptas tempore nostrum laboriosam voluptatibus inventore minima quas sunt quibusdam saepe eos quaerat consequuntur repellat voluptatum similique repudiandae ipsum enim qui eligendi.
-        </Text>
-        <Accordion allowToggle>
-          <AccordionItem>
-            <h2>
-            <AccordionButton _expanded={{ bg: "orange.400" }}>
-                <Box flex='1' textAlign='left' fontSize='20px' color={isDark ? 'white' : "black"}>
-                  see more
-                </Box>
-                <AccordionIcon />
-              </AccordionButton>
-            </h2>
-            <AccordionPanel>
-              <AccordionContent isDark={isDark} />
-            </AccordionPanel>
-          </AccordionItem>
-        </Accordion>
-      </Card>
-    </Box>
+        <HStack spacing='20px'>
+          <Link href="https://github.com" isExternal>
+            <IconButton icon={<FaGithub />} aria-label="GitHub" />
+          </Link>
+          <Link href="https://linkedin.com" isExternal>
+            <IconButton icon={<FaLinkedin />} aria-label="LinkedIn" />
+          </Link>
+          <Link href="mailto:email@example.com">
+            <IconButton icon={<FaEnvelope />} aria-label="Email" />
+          </Link>
+        </HStack>
+      </VStack>
+    </MotionBox>
   );
 };
 
